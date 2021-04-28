040a6d8136e4c57bbf5c9d284dd57060f45e67b1
Raffael Luna <raffael.luna@gmail.com>
2021-04-28 08:43:22 -0300

    finished type classes class

---
+++ type-classes/TypeWrapping.hs
@@ -0,0 +1,28 @@
+data Quaternion = Q 
+    {
+        qR :: Double
+        , qI :: Double
+        , qJ :: Double
+        , qK :: Double
+    }
+
+newtype PrettyQuaternion = PrettyQuaternion { unPrettyQuaternion :: Quaternion }
+instance Show PrettyQuaternion where
+    show q = let q' = unPrettyQuaternion q in "(" ++
+        show (qR q') ++ " + " ++
+        show (qI q') ++ "i + " ++
+        show (qJ q') ++ "j + " ++
+        show (qK q') ++ "k)"
+
+newtype UglyQuaternion = UglyQuaternion { unUglyQuaternion :: Quaternion }
+instance Show UglyQuaternion where
+    show q = let q' = unUglyQuaternion q in "(" ++
+        show (qR q') ++ "," ++
+        show (qI q') ++ "," ++
+        show (qJ q') ++ "," ++
+        show (qK q') ++ ")"
+
+main :: IO ()
+main = do
+    print $ PrettyQuaternion (Q 1 2 3 4)
+    print $ UglyQuaternion (Q 1 2 3 4)
